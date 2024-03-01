cb6270209e5cd07a72b36964063dd8b78b3a10d0
jorgealain.maza.contractor <jorgealain.maza.contractor@bbva.com>
2024-03-01 16:47:44 -0600

    avance

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
     
 @app.route("/")
-def insertion_sort():
+def principal():
     return jsonify({"mensaje": "Datos recibidos correctamente."}), 200
 
 
--- startup-python.sh
+++ startup-python.sh
@@ -1,8 +1,18 @@
 sudo apt update
-sudo apt install python3-pip nginx git python3-gunicorn python3-flask-y
-# pip3 install flask
-# pip install gunicorn
+sudo apt install python3-pip nginx git python3-venv ufw -y
+python3 -m venv venv
+source venv/bin/activate
+echo 'server {
+         listen 80;
+         location / {
+                proxy_pass http://127.0.0.1:8000;
+                proxy_set_header Host $host;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+         }
+ }' | sudo tee /etc/nginx/sites-enabled/flask_app > /dev/null
+sudo unlink /etc/nginx/sites-enabled/default
+sudo nginx -s reload
 git clone https://github.com/Maza-Alain/urlmap-crud-python.git
 cd urlmap-crud-python
-pip install
-gunicorn --workers=3 app:app 
+pip install -r requirements.txt
+gunicorn --workers=3 app:app --daemon
